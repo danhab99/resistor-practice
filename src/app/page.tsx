76fdde3d93bfbfe47180574fb728e383b387b5d2
Dan Habot <dan.habot@gmail.com>
2023-09-03 00:39:59 -0400

    created app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,211 @@
-import Image from 'next/image'
+"use client"; // This is a client component
+
+import React, { useState, useCallback, useReducer } from "react";
+
+const backgroundColor = "#9c9459";
+
+const Resistor = ({ colors }: { colors: string[] }) => {
+  // Define the SVG dimensions and resistor dimensions
+  const svgWidth = 300;
+  const svgHeight = 100;
+  const resistorWidth = svgWidth - 40;
+  const resistorHeight = 40;
+  const leadWidth = 30;
+  const leadHeight = 30;
+  const bulbousRadius = 10; // Adjust the bulbous end radius
+
+  // Calculate the space available for each color band
+  const bandWidth = (resistorWidth - 2 * leadWidth) / colors.length;
+
+  return (
+    <svg width={svgWidth} height={svgHeight} xmlns="http://www.w3.org/2000/svg">
+      {/* Left bulbous end */}
+      <ellipse
+        cx={(svgWidth - resistorWidth) / 2}
+        cy={svgHeight / 2}
+        rx={bulbousRadius}
+        ry={resistorHeight / 2}
+        fill={backgroundColor}
+      />
+
+      {/* Right bulbous end */}
+      <ellipse
+        cx={(svgWidth + resistorWidth) / 2}
+        cy={svgHeight / 2}
+        rx={bulbousRadius}
+        ry={resistorHeight / 2}
+        fill={backgroundColor}
+      />
+
+      {/* Resistor body */}
+      <rect
+        x={(svgWidth - resistorWidth) / 2 + bulbousRadius}
+        y={(svgHeight - resistorHeight) / 2}
+        width={resistorWidth - 2 * bulbousRadius}
+        height={resistorHeight}
+        fill={backgroundColor}
+      />
+
+      {/* Left lead */}
+      <rect
+        x={(svgWidth + resistorWidth) / 2 - leadWidth}
+        y={(svgHeight - leadHeight) / 2}
+        width={leadWidth}
+        height={leadHeight}
+        fill={backgroundColor}
+      />
+
+      {/* Right lead */}
+      <rect
+        x={(svgWidth - resistorWidth) / 2}
+        y={(svgHeight - leadHeight) / 2}
+        width={leadWidth}
+        height={leadHeight}
+        fill={backgroundColor}
+      />
+
+      {/* Color bands */}
+      {colors.map((color, index) => (
+        <rect
+          key={index}
+          x={(svgWidth - resistorWidth) / 1.5 + leadWidth + index * bandWidth}
+          y={(svgHeight - resistorHeight) / 2}
+          width={bandWidth / 2}
+          height={resistorHeight}
+          fill={color}
+        />
+      ))}
+    </svg>
+  );
+};
+
+function randomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+type ChallengeType = {
+  numbers: {
+    base: number;
+    factor: number;
+    tolerance: number;
+  };
+  colors: string[];
+};
+
+const ColorMap: Record<string, string> = {
+  "0": "black",
+  "1": "brown",
+  "2": "red",
+  "3": "orange",
+  "4": "yellow",
+  "5": "green",
+  "6": "blue",
+  "7": "violet",
+  "8": "grey",
+  "9": "white",
+};
+
+const generateChallenge = (_: ChallengeType, __: any): ChallengeType => {
+  const base = randomInt(1, 99);
+  const factor = randomInt(1, 10);
+  const tolerance = randomInt(1, 10);
+
+  var colors: string[] = [];
+
+  const b = base.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+
+  for (let c of b) {
+    colors.push(ColorMap[c]);
+  }
+
+  colors.push(ColorMap[factor]);
+  colors.push(backgroundColor);
+  colors.push(ColorMap[tolerance]);
+
+  return {
+    numbers: {
+      base,
+      factor,
+      tolerance,
+    },
+    colors,
+  };
+};
 
 export default function Home() {
+  const [challenge, newChallenge] = useReducer(
+    generateChallenge,
+    {} as ChallengeType,
+    () => generateChallenge({} as ChallengeType, null),
+  );
+
+  const [base, setBase] = useState(0);
+  const [factor, setFactor] = useState(0);
+  const [tolerance, setTolerance] = useState(0);
+
+  const reset = () => {
+    newChallenge(null);
+    setBase(0);
+    setFactor(0);
+    setTolerance(0);
+  };
+
+  const baseCorrect = challenge.numbers.base == base;
+  const factorCorrect = challenge.numbers.factor == factor;
+  const toleranceCorrect = challenge.numbers.tolerance == tolerance;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex flex-col items-center p-8">
+      <button onClick={reset}>New Challenge</button>
+      {challenge.colors ? <Resistor colors={challenge.colors} /> : null}
+      <label>Value (ohms)</label>
+      <input
+        type="number"
+        className="p-2 rounded-full px-4"
+        style={{
+          backgroundColor: baseCorrect ? "green" : "red",
+        }}
+        value={base}
+        max={99}
+        min={0}
+        onChange={(e) => setBase(parseInt(e.target.value))}
+      />
+      <label>Factor</label>
+      <input
+        type="number"
+        className="p-2 rounded-full px-4"
+        style={{
+          backgroundColor: factorCorrect ? "green" : "red",
+        }}
+        value={factor}
+        max={10}
+        min={0}
+        onChange={(e) => setFactor(parseInt(e.target.value))}
+      />
+      <label>Tolerance (%)</label>
+      <input
+        type="number"
+        className="p-2 rounded-full px-4"
+        style={{
+          backgroundColor: toleranceCorrect ? "green" : "red",
+        }}
+        value={tolerance}
+        max={10}
+        min={0}
+        onChange={(e) => setTolerance(parseInt(e.target.value))}
+      />
+      <details className="w-fit">
+        <summary className="text-center">Hint</summary>
+        <img
+          src="https://www.te.com/content/dam/te-com/images/corporate/marketing/global/infographics/resistor-color-code-bands-3-4-1024.png?w=940"
+          className="md:w-1/2 mx-auto"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </details>
+    </div>
+  );
 }
