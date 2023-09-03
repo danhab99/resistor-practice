beb1c743ab2c38edab2ab260621e3646e2149e9e
Dan Habot <dan.habot@gmail.com>
2023-09-03 01:07:13 -0400

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Resistor Practice',
-  description: 'Practice reading resistor codes',
-}
+  title: "Resistor Practice",
+  description: "Practice reading resistor codes",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Resistor Practice</title>
+        <meta
+          name="google-site-verification"
+          content="y-Rx7OQIIqGj5ebGsRWAhhgQTOFisE_WKbcQpE5s7SA"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
